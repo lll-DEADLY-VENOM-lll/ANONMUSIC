cfc33dd7dd67343548da109c60edfb3417a9d73c
野买 <madhuripatel509@gmail.com>
2025-09-18 16:18:59 +0530

    Update call.py

--- AnonMusic/core/call.py
+++ AnonMusic/core/call.py
@@ -10,7 +10,8 @@ from pytgcalls.exceptions import (
     NoActiveGroupCall,
 )
 from ntgcalls import TelegramServerError
-from pytgcalls.types import Update, StreamEnded
+from pytgcalls.types import Update
+#from pytgcalls.types import Update, StreamEnded
 from pytgcalls import filters as fl
 from pytgcalls.types import AudioQuality, VideoQuality
 from pytgcalls.types import MediaStream,ChatUpdate
