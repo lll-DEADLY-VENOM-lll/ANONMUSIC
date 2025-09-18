66350a4113456966a7cb1a83afe49db0ba98d216
野买 <madhuripatel509@gmail.com>
2025-09-18 19:06:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # Git repo details (for updates & deployment)
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Vibe-Bots/SanataniTechApi")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/VNI0X/ANONMUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")  # Only required for private repos
 
