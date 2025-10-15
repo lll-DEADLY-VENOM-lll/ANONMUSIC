1fdd70bb3671aa1d8f1df995457ce8deee800ec9
野买 <madhuripatel509@gmail.com>
2025-10-15 09:31:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # YouTube streaming proxy and key (used in API backend)
-BASE_API_URL = getenv("API_URL", "https://ytmp3.taitanapi.workers.dev")
-BASE_API_KEY = getenv("API_KEY", "TaitanXApi")
+YTPROXY_URL = getenv("YTPROXY_URL", "https://tgapi.xbitcode.com")
+YT_API_KEY = getenv("YT_API_KEY", "xbit_AY2JXTDQPYPRW6FD66GSRV")
 DOWNLOADS_DIR = getenv("DOWNLOADS_DIR", "downloads")
 
 BOT_DOCS =  getenv("BOT_DOCS", "https://radha-music.vercel.app")
