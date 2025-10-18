b8af0af62c84236edeedf0ad2f76750f4dd967e9
野买 <madhuripatel509@gmail.com>
2025-10-18 23:29:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # YouTube streaming proxy and key (used in API backend)
 YTPROXY_URL = getenv("YTPROXY_URL", "https://tgapi.xbitcode.com")
-YT_API_KEY = getenv("YT_API_KEY", "xbit_ZO590ZM63DM1AU7GVEU4LM)
+YT_API_KEY = getenv("YT_API_KEY", "xbit_ZO590ZM63DM1AU7GVEU4LM")
 DOWNLOADS_DIR = getenv("DOWNLOADS_DIR", "downloads")
 
 BOT_DOCS =  getenv("BOT_DOCS", "https://radha-music.vercel.app")
